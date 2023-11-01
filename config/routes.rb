bacee2d7a2c176e9d2232d345727a14af2e92b2e
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-11-01 20:59:56 +0430

    Remove commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  # MY CODE
-  # get '/users', to: "users#index"
-  # get '/users/:id', to: "users#show"
-  # get '/users/:user_id/posts', to: "posts#index"
-  # get '/users/:user_id/posts/:id', to: "posts#show"
-  
-  #CODE REVIEWER optional comment
-  # [OPTIONAL] While this is going to work fine, I will encourage you to take advantage of resources by having something like:
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
